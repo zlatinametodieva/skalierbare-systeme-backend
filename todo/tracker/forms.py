d83d36ad9f25048721f91702adb7d51ce266332d
zlatinametodieva <64097972+zlatinametodieva@users.noreply.github.com>
2021-05-13 15:39:47 +0200

    added edit and delete functionalities

--- todo/tracker/forms.py
+++ todo/tracker/forms.py
@@ -4,3 +4,14 @@ from django import forms
 class AddTodoForm(forms.Form):
     content = forms.CharField(label='TODO', max_length=159)
     deadline = forms.DateField(label='Deadline')
+
+
+class DeleteTodoForm(forms.Form):
+    id = forms.IntegerField(label='id')
+
+
+class EditTodoForm(forms.Form):
+    n = forms.IntegerField(label='id')
+    cont = forms.CharField(label='TODO', max_length=159)
+    dline = forms.DateField(label='Deadline')
+    prog = forms.IntegerField(label='Progress')
--- todo/tracker/views.py
+++ todo/tracker/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from .models import Todo
-from .forms import AddTodoForm
+from .forms import AddTodoForm, EditTodoForm, DeleteTodoForm
 
 
 def index_view(request):
@@ -10,11 +10,21 @@ def index_view(request):
     context = {'todo_list': zip(range(1, len(todo_list) + 1), todo_list)}
 
     if request.method == 'POST':
-        form = AddTodoForm(request.POST)
-        if form.is_valid():
-            todo = Todo(content=form.cleaned_data['content'], deadline=form.cleaned_data['deadline'])
+        add_todo_form = AddTodoForm(request.POST)
+        edit_todo_form = EditTodoForm(request.POST)
+        delete_todo_form = DeleteTodoForm(request.POST)
+
+        if add_todo_form.is_valid():
+            todo = Todo(content=add_todo_form.cleaned_data['content'], deadline=add_todo_form.cleaned_data['deadline'])
             todo.save()
+        if edit_todo_form.is_valid():
+            Todo.objects.filter(id=edit_todo_form.cleaned_data['n']).update(content=edit_todo_form.cleaned_data['cont'],
+                                                                            deadline = edit_todo_form.cleaned_data['dline'],
+                                                                            progress = edit_todo_form.cleaned_data['prog'])
+        if delete_todo_form.is_valid():
+            Todo.objects.filter(id=delete_todo_form.cleaned_data['id']).delete()
         return HttpResponseRedirect('/')
+
     else:
         return render(request, 'index.html', context)
 
